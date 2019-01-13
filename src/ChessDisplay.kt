4103aca005665ea7347d9f6c95580683f7c3d80c
Kristjan <nigathr@gmail.com>
2019-01-14 00:32:31 +0100

    Added Unicode chess symbols

--- src/ChessDisplay.kt
+++ src/ChessDisplay.kt
@@ -12,25 +12,14 @@ class ChessDisplay(val canvas: HTMLCanvasElement, val infoArea: HTMLDivElement) 
             context.fillStyle = "black"
             context.font = fieldSize.toString() + "px arial"
             context.textBaseline = CanvasTextBaseline.TOP
-            if(piece?.player == ChessPlayer.Black) {
-                when (piece.type) {
-                    ChessPieceType.King -> context.fillText("K", 0.0, 0.0)
-                    ChessPieceType.Queen -> context.fillText("Q", 0.0, 0.0)
-                    ChessPieceType.Knight -> context.fillText("N", 0.0, 0.0)
-                    ChessPieceType.Rook -> context.fillText("R", 0.0, 0.0)
-                    ChessPieceType.Bishop -> context.fillText("B", 0.0, 0.0)
-                    ChessPieceType.Pawn -> context.fillText("P", 0.0, 0.0)
-                }
-            }
-            else {
-                when (piece?.type) {
-                    ChessPieceType.King -> context.strokeText("K", 0.0, 0.0)
-                    ChessPieceType.Queen -> context.strokeText("Q", 0.0, 0.0)
-                    ChessPieceType.Knight -> context.strokeText("N", 0.0, 0.0)
-                    ChessPieceType.Rook -> context.strokeText("R", 0.0, 0.0)
-                    ChessPieceType.Bishop -> context.strokeText("B", 0.0, 0.0)
-                    ChessPieceType.Pawn -> context.strokeText("P", 0.0, 0.0)
-                }
+            val isBlack = piece?.player == ChessPlayer.Black
+            when (piece?.type) {
+                ChessPieceType.King -> context.fillText(if(isBlack) "♚" else "♔", 0.0, 0.0)
+                ChessPieceType.Queen -> context.fillText(if(isBlack) "♛" else "♕", 0.0, 0.0)
+                ChessPieceType.Knight -> context.fillText(if(isBlack) "♞" else "♘", 0.0, 0.0)
+                ChessPieceType.Rook -> context.fillText(if(isBlack) "♜" else "♖", 0.0, 0.0)
+                ChessPieceType.Bishop -> context.fillText(if(isBlack) "♝" else "♗", 0.0, 0.0)
+                ChessPieceType.Pawn -> context.fillText(if(isBlack) "♟" else "♙", 0.0, 0.0)
             }
         }
         squareDisplay.display(game.board, getColor, draw)
