4a63defbfa263c8b2b79d794391a9d8372580dda
Thomas-Wang12 <84743398+Thomas-Wang12@users.noreply.github.com>
2023-03-21 23:28:12 -0400

    Update AlysBoardCreator.kt

--- src/Alys/AlysBoardCreator.kt
+++ src/Alys/AlysBoardCreator.kt
@@ -4,7 +4,7 @@ import kotlin.random.Random
 class AlysBoardCreator(width: Int, height: Int, val seed: Int) {
 	val board: Grid<AlysField?> = Grid(width, height, { _, _ -> null })
 	var numberOfCenters: Int = 3
-	var landFraction: Double = 0.8
+	var landFraction: Double = 0.96
 
 	fun generateLand() {
 		val random = Random(seed)
